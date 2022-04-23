ef76e7a9028ffaa4b152b581cbb349201fdfcfea
nshadybekov <nshadybekov@adsterra.com>
2022-04-23 17:34:15 +0300

    api auth

--- django_rest/tutorial/quickstart/models.py
+++ django_rest/tutorial/quickstart/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
-# Create your models here.
+
+owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+highlighted = models.TextField()
+
+def save(self, *args, **kwargs):
+    """
+    Use the `pygments` library to create a highlighted HTML
+    representation of the code snippet.
+    """
+    lexer = get_lexer_by_name(self.language)
+    linenos = 'table' if self.linenos else False
+    options = {'title': self.title} if self.title else {}
+    formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                              full=True, **options)
+    self.highlighted = highlight(self.code, lexer, formatter)
+    super().save(*args, **kwargs)
