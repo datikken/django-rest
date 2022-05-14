9aef46d4a5c49b0c58b5787770281b866f2204e9
nshadybekov <nshadybekov@adsterra.com>
2022-05-14 12:21:07 +0300

    customizing django admin

--- django/polls/models.py
+++ django/polls/models.py
@@ -1,6 +1,9 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from random import randint
+from django.contrib import admin
+
 
 # >>> q = Question.objects.get(pk=1)
 # >>> q.was_published_recently()
@@ -8,16 +11,27 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Fresh new?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def random_int(self):
+        return randint(1, 10)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
